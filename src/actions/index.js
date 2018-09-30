0798c658563b2f5598b6b27e2e4424c222af1559
ilaydagurbak <ilaydagurbak@gmail.com>
2018-09-30 20:01:29 +0300

    redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 const API_KEY = '3ccf43cffd9a4d09ed988be8d209fec6'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
@@ -8,8 +9,10 @@ export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`
     const request = axios.get(url)
 
+    console.log('Request:', request)
+
     return {
         type: FETCH_WEATHER,
         payload: request
     }
-}
+} 
